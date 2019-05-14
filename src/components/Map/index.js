1a52a6fd860125dd217054131884ac05e242f465
gustavoperito <gustavo.aperito@hotmail.com>
2019-05-14 20:39:38 -0300

    asdasd

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MapView from 'react-native-maps'
 import { View } from 'react-native';
+import Search from '../Search';
 
 export default class Map extends Component {
    state = {
@@ -39,6 +40,7 @@ export default class Map extends Component {
             loadingEnabled
          >
          </MapView>
+         <Search/>
       </View>;
    }
 }
